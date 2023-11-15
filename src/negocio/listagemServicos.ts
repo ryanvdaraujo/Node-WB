a67c09f37fb11e05319d10dc1f2d73a78c8e4f74
Ryan <negoryanaraujo@gmail.com>
2023-11-15 18:35:08 -0300

    Correção de listagem de clientes

--- src/negocio/listagemServicos.ts
+++ src/negocio/listagemServicos.ts
@@ -4,15 +4,17 @@ import Listagem from "./listagem";
 
 export default class ListagemServicos extends Listagem {
     private servicos: Array<Servico>
-    constructor(produtos: Array<Servico>){
+    constructor(servicos: Array<Servico>){
         super()
-        this.servicos = servico
+        this.servicos = servicos
     }
     public listar(): void {
         console.log(`\nLista de todos os produtos: \n`);
         this.servicos.forEach(servico => {
             console.log(`Nome do serviço: ` + servico.nome);
             console.log(`Preço do serviço: R$` + servico.valor);
+            console.log(`ID: ` + servico.index);
+            console.log(`**********************************`)
         })
     }
 }
