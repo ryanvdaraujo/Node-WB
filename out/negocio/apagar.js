774846f02692e6afff41eac0ba7582ec85910e4a
Ryan <negoryanaraujo@gmail.com>
2023-11-23 08:27:35 -0300

    remoção do node modules

--- out/negocio/apagar.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Apagar = /** @class */ (function () {
-    function Apagar() {
-    }
-    return Apagar;
-}());
-exports.default = Apagar;
--- out/negocio/busca.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Busca = /** @class */ (function () {
-    function Busca() {
-    }
-    return Busca;
-}());
-exports.default = Busca;
