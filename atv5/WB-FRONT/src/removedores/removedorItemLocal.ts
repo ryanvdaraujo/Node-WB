a7f7bd4d1e7e42efdac0cc5eb584cd49303a7696
Ryan <negoryanaraujo@gmail.com>
2023-12-03 22:56:19 -0300

    Organização em branch

--- atv5/WB-FRONT/src/removedores/removedorItemLocal.ts
+++
@@ -1,8 +0,0 @@
-import RemovedorLocal from "./removedorLocal";
-
-export default class RemovedorItemLocal implements RemovedorLocal {
-    public remover(objeto: Object[], id: string): Object[] {
-        let itens = objeto.filter(elemento => elemento['id'] !== id)
-        return itens
-    }
-}
